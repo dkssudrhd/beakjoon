f433990bb190e0acadb7b229cb9293ca848081b1
dkssu <dkssudrhd@naver.com>
2024-08-16 01:22:47 +0900

    feature : 15656

---
+++ silver/Beak_15656.java
@@ -0,0 +1,50 @@
+package silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Beak_15656 {
+    static int n, m;
+    static int[] arr;
+    static int[] numberList;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        numberList = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            numberList[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(numberList);
+
+
+        arr = new int[m];
+        dfs(0, 0);
+        System.out.print(sb);
+        br.close();
+    }
+
+    private static void dfs(int depth, int start){
+        if(depth == m){
+            for(int i = 0; i < arr.length; i++){
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for(int i=start ; i< numberList.length; i++){
+            arr[depth] = numberList[i];
+            dfs(depth+1, i);
+        }
+    }
+}
