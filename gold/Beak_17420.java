044d6e8c3250586ad2c91a4fc5100c358677cb98
dkssu <dkssudrhd@naver.com>
2024-12-18 21:13:28 +0900

    feature : 17420번

--- gold/Beak_17420.java
+++ gold/Beak_17420.java
@@ -1,87 +1,84 @@
 package gold;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
-class Box{
-    int a;
-    int b;
+class Task {
+    int A;
+    int B;
 
-    public Box(int a, int b){
-        this.a = a;
-        this.b = b;
-    }
-
-    public int aSetting(int nowDay){
-        int k = (nowDay - a)/30;
-        if((nowDay-a)%30 != 0){
-            k++;
-        }
-        a += 30*k;
-        System.out.println("now a: " + a + " now b : " + b);
-        return k;
+    public Task(int A, int B) {
+        this.A = A;
+        this.B = B;
     }
 }
 
-
-/**
- * 문제의 핵심이 기한이 가장 적게 남은 기프티콘만 사용할 수 있다.
- * 1. 먼저 입력값 받아오기
- * 2. 남은 기간으로 정렬
- * 3. poll 기간이 안되면 다시 넣기 유효기간 추가하고
- */
 public class Beak_17420 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        long sum = 0;
 
-        StringTokenizer stA = new StringTokenizer(br.readLine());
-        StringTokenizer stB = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
-        PriorityQueue<Box> pq = new PriorityQueue<>((o1, o2) -> o1.a - o2.a);
-        PriorityQueue<Integer> pqB = new PriorityQueue<>();
-        for (int i = 0; i < n; i++) {
-            int A = Integer.parseInt(stA.nextToken());
-            int B = Integer.parseInt(stB.nextToken());
-            pqB.add(B);
-            pq.offer(new Box(A, B));
+        int[] A = new int[N];
+        int[] B = new int[N];
+
+        String[] aInput = br.readLine().split(" ");
+        String[] bInput = br.readLine().split(" ");
+
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(aInput[i]);
         }
 
-        int nowB = pqB.poll();
-        List<Box> boxes = new ArrayList<>();        // 같은 기한의
-        int boxesA = -1;
+        for (int i = 0; i < N; i++) {
+            B[i] = Integer.parseInt(bInput[i]);
+        }
 
-        while(!pq.isEmpty()){
-            Box box = pq.poll();
+        List<Task> arr = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            arr.add(new Task(A[i], B[i]));
+        }
 
-            if(box.a < nowB){                       // 기한이 다된 기프티콘
-                sum += box.aSetting(nowB);
-                pq.offer(box);
-            }else if(box.b == nowB){                //  가장 적게 남은 기프티콘일 경우
-                if(pqB.isEmpty())
-                    break;
-                nowB = pqB.poll();
-                for(Box b : boxes){
-                    sum += b.aSetting(box.a);
-                    pq.offer(b);
+        // TASK 구조체를 B로 먼저 정렬하고, B가 같다면 A로 정렬
+        Collections.sort(arr, new Comparator<Task>() {
+            @Override
+            public int compare(Task t1, Task t2) {
+                if (t1.B == t2.B) {
+                    return Integer.compare(t1.A, t2.A);
                 }
-                boxes.clear();
-            }else if(boxes.isEmpty()){              // 사용할 수는 있는
-                boxes.add(box);
-                boxesA = box.a;
-            }else if(boxesA < box.a){
-                for(Box b : boxes){
-                    sum += b.aSetting(box.a);
-                    pq.offer(b);
+                return Integer.compare(t1.B, t2.B);
+            }
+        });
+
+        long answer = 0; // 정수 범위를 넘어설 수 있으므로 long 사용
+        int prevMax = arr.get(0).B; // 이전 구간의 최대 B 값
+        int curMax = -1; // 현재 구간에서의 A 최대값
+
+        for (int i = 0; i < N; i++) {
+            if (prevMax > arr.get(i).A) {
+                // 이전 구간의 최댓값 보다 현재 A값이 작을 경우
+                if (prevMax < arr.get(i).B) {
+                    prevMax = arr.get(i).B;
                 }
-                boxes.clear();
+
+                int cnt = ((prevMax - arr.get(i).A) + 29) / 30; // 30일 단위로 보정
+                arr.get(i).A += (cnt * 30);
+                answer += cnt;
+            }
+
+            // 현재 구간에서의 최대 A값 계산
+            curMax = Math.max(curMax, arr.get(i).A);
+
+            // 구간이 바뀔 때 이전 구간의 최대값을 갱신
+            if ((i + 1) < N && arr.get(i).B != arr.get(i + 1).B) {
+                prevMax = curMax;
             }
         }
-        System.out.println(sum);
 
+        System.out.println(answer);
     }
 }
